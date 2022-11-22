1243aef6ca3649751109ecec67f342dc91c7ca83
prernagarsole <99928364+prernagarsole@users.noreply.github.com>
2022-11-21 22:10:47 -0800

    Update myTFDistributedCustomTrainer.py

--- TFClassifier/myTFDistributedCustomTrainer.py
+++ TFClassifier/myTFDistributedCustomTrainer.py
@@ -12,22 +12,24 @@ import time
 import os
 print(tf.__version__)
 
-from Datasetutil.TFdatasetutil import loadtfds, loadkerasdataset
+from TFClassifier.Datasetutil.TFdatasetutil import loadTFdataset,loadkerasdataset,loadimagefolderdataset
+from TFClassifier.Datasetutil.TFdatasetutil import loadtfds#loadtfds, loadkerasdataset, loadimagefolderdataset
+from TFClassifier.myTFmodels.CNNsimplemodels import createCNNsimplemodel
 
 model = None 
 # import logger
 
 parser = configargparse.ArgParser(description='myTFDistributedClassify')
-parser.add_argument('--tfds_dataname', type=str, default='mnist',
-                    help='path to get data')
+parser.add_argument('--data_name', type=str, default='fashionMNIST',
+                    help='data name mnist, fashionMNIST')
+parser.add_argument('--data_type', default='kerasdataset', choices=['tfds', 'kerasdataset', 'imagefolder', 'TFrecord'],
+                    help='the type of data')  # gs://cmpelkk_imagetest/*.tfrec
 parser.add_argument('--data_path', type=str, default='/home/kaikai/.keras/datasets/flower_photos',
                     help='path to get data')
 parser.add_argument('--save_path', type=str, default='./outputs/fashion',
                     help='path to save the model')
-parser.add_argument('--data_type', default='folder', choices=['folder', 'TFrecord'],
-                    help='the type of data')  # gs://cmpelkk_imagetest/*.tfrec
 # network
-parser.add_argument('--model_name', default='depth', choices=['disparity', 'depth'],
+parser.add_argument('--model_name', default='cnnsimple1', choices=['cnnsimple1', 'cnnsimple2'],
                     help='the network')
 parser.add_argument('--arch', default='Tensorflow', choices=['Tensorflow', 'Pytorch'],
                     help='Model Name, default: Tensorflow.')
@@ -81,19 +83,6 @@ def create_model(strategy,numclasses, metricname='accuracy'):
                       optimizer=tf.keras.optimizers.Adam(),
                       metrics=[metricname])
         return model
-
-def create_model2():
-  model = tf.keras.Sequential([
-      tf.keras.layers.Conv2D(32, 3, activation='relu'),
-      tf.keras.layers.MaxPooling2D(),
-      tf.keras.layers.Conv2D(64, 3, activation='relu'),
-      tf.keras.layers.MaxPooling2D(),
-      tf.keras.layers.Flatten(),
-      tf.keras.layers.Dense(64, activation='relu'),
-      tf.keras.layers.Dense(10)
-    ])
-
-  return model
 
 # Function for decaying the learning rate.
 # You can define any decay function you need.
@@ -154,119 +143,76 @@ def main():
         BATCH_SIZE = BATCH_SIZE_PER_REPLICA * strategy.num_replicas_in_sync
 
 
-    train_data, test_data, num_train_examples, num_test_examples =loadkerasdataset()
-    #train_data, test_data, num_train_examples, num_test_examples = loadtfds(args.tfds_dataname)
-    print(num_train_examples)
+    train_ds, val_ds, class_names, imageshape = loadTFdataset('fashionMNIST', args.data_type)
+    #train_ds, test_data, num_train_examples, num_test_examples, class_names=loadimagefolderdataset('flower')
+    #train_ds, val_ds, num_train_examples, num_test_examples,class_names,imageshape =loadkerasdataset('fashionMNIST')
+    #train_data, test_data, num_train_examples, num_test_examples = loadtfds('tf_flowers')
 
     # train_data, test_data, num_train_examples, num_test_examples = loadtfds(
     #     args.tfds_dataname)
-    # Apply this function to the training and test data, shuffle the training data, and batch it for training.
-    # This dataset fills a buffer with buffer_size elements, then randomly samples elements from this buffer, replacing the selected elements with new elements. For perfect shuffling, a buffer size greater than or equal to the full size of the dataset is required.
-    # https://www.tensorflow.org/api_docs/python/tf/data/Dataset#shuffle
-    # train_ds = train_data.map(scale).cache().shuffle(
-    #     BUFFER_SIZE).batch(BATCH_SIZE)
-    # val_ds = test_data.map(scale).batch(BATCH_SIZE)
-    train_ds = train_data.cache().shuffle(
-        BUFFER_SIZE).batch(BATCH_SIZE)
-    val_ds = test_data.batch(BATCH_SIZE)
 
-    train_dist_dataset = strategy.experimental_distribute_dataset(train_ds)
-    test_dist_dataset = strategy.experimental_distribute_dataset(val_ds)
+    #Tune for performance
+    AUTOTUNE = tf.data.AUTOTUNE
+    train_ds = train_ds.cache().shuffle(BUFFER_SIZE).prefetch(buffer_size=AUTOTUNE)
+    val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
+
+    numclasses=len(class_names)
+
+    global model
+    metricname='accuracy'
+    metrics=[metricname]
+    with strategy.scope():
+        model = createCNNsimplemodel(args.model_name, numclasses, imageshape, metrics)
+    #model = create_model(strategy,numclasses, metricname)
+    model.summary()
 
     # Define the checkpoint directory to store the checkpoints
     checkpoint_dir = args.save_path #'./training_checkpoints'
     # Name of the checkpoint files
     checkpoint_prefix = os.path.join(checkpoint_dir, "ckpt_{epoch}")
 
-    with strategy.scope():
-        # Set reduction to `none` so we can do the reduction afterwards and divide by
-        # global batch size.
-        loss_object = tf.keras.losses.SparseCategoricalCrossentropy(
-            from_logits=True,
-            reduction=tf.keras.losses.Reduction.NONE)
-        def compute_loss(labels, predictions):
-            per_example_loss = loss_object(labels, predictions)
-            return tf.nn.compute_average_loss(per_example_loss, global_batch_size=BATCH_SIZE)
+    callbacks = [
+        tf.keras.callbacks.TensorBoard(log_dir=args.save_path),
+        tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_prefix,
+                                        save_weights_only=True),
+        tf.keras.callbacks.LearningRateScheduler(learningratefn),
+        PrintLR()
+    ]
 
-    with strategy.scope():
-        test_loss = tf.keras.metrics.Mean(name='test_loss')
+    #steps_per_epoch = num_train_examples // BATCH_SIZE  # 2936 is the length of train data
+    #print("steps_per_epoch:", steps_per_epoch)
+    start_time = time.time()
+    #train the model 
+    history = model.fit(train_ds, validation_data=val_ds, epochs=args.epochs, callbacks=callbacks)
+    # history = model.fit(train_ds, validation_data=val_ds,
+    #                     steps_per_epoch=steps_per_epoch, epochs=EPOCHS, callbacks=[lr_callback])
 
-        train_accuracy = tf.keras.metrics.SparseCategoricalAccuracy(
-            name='train_accuracy')
-        test_accuracy = tf.keras.metrics.SparseCategoricalAccuracy(
-            name='test_accuracy')
+    valmetricname="val_"+metricname
+    final_accuracy = history.history[valmetricname][-5:]
+    print("FINAL ACCURACY MEAN-5: ", np.mean(final_accuracy))
+    print("TRAINING TIME: ", time.time() - start_time, " sec")
 
-    global model
-    # model, optimizer, and checkpoint must be created under `strategy.scope`.
-    with strategy.scope():
-        model = create_model2()
+    plot_history(history, metricname, valmetricname)
+    hi='./outputs/fashions/tfrest/'
+    version = 1
+    export_path = os.path.join(hi, str(version))
 
-        optimizer = tf.keras.optimizers.Adam()
-
-        checkpoint = tf.train.Checkpoint(optimizer=optimizer, model=model)
-        
-    def train_step(inputs):
-        images, labels = inputs
-
-        with tf.GradientTape() as tape:
-            predictions = model(images, training=True)
-            loss = compute_loss(labels, predictions)
-
-        gradients = tape.gradient(loss, model.trainable_variables)
-        optimizer.apply_gradients(zip(gradients, model.trainable_variables))
-
-        train_accuracy.update_state(labels, predictions)
-        return loss 
-
-    def test_step(inputs):
-        images, labels = inputs
-
-        predictions = model(images, training=False)
-        t_loss = loss_object(labels, predictions)
-
-        test_loss.update_state(t_loss)
-        test_accuracy.update_state(labels, predictions)
-    
-    # `run` replicates the provided computation and runs it
-    # with the distributed input.
-    @tf.function
-    def distributed_train_step(dataset_inputs):
-        per_replica_losses = strategy.run(train_step, args=(dataset_inputs,))
-        return strategy.reduce(tf.distribute.ReduceOp.SUM, per_replica_losses,
-                                axis=None)
-
-    @tf.function
-    def distributed_test_step(dataset_inputs):
-        return strategy.run(test_step, args=(dataset_inputs,))
-    
-    for epoch in range(args.epochs):
-        # TRAIN LOOP
-        total_loss = 0.0
-        num_batches = 0
-        for x in train_dist_dataset:
-            total_loss += distributed_train_step(x)
-            num_batches += 1
-        train_loss = total_loss / num_batches
-
-        # TEST LOOP
-        for x in test_dist_dataset:
-            distributed_test_step(x)
-
-        if epoch % 2 == 0:
-            checkpoint.save(checkpoint_prefix)
-
-        template = ("Epoch {}, Loss: {}, Accuracy: {}, Test Loss: {}, "
-                    "Test Accuracy: {}")
-        print (template.format(epoch+1, train_loss,
-                                train_accuracy.result()*100, test_loss.result(),
-                                test_accuracy.result()*100))
-
-        test_loss.reset_states()
-        train_accuracy.reset_states()
-        test_accuracy.reset_states()
-    
     #Export the graph and the variables to the platform-agnostic SavedModel format. After your model is saved, you can load it with or without the scope.
     model.save(args.save_path, save_format='tf')
+    
+    tf.keras.models.save_model(
+    model,
+    export_path,
+    overwrite=True,
+    include_optimizer=True,
+    save_format=None,
+    signatures=None,
+    options=None
+    )
+    print("Done!!!!!! Saved to -->  ",hi)
+
+    eval_loss, eval_acc = model.evaluate(val_ds)
+    print('Eval loss: {}, Eval Accuracy: {}'.format(eval_loss, eval_acc))
 
 if __name__ == '__main__':
     main()
